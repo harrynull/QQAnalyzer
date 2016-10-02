ab8c8537a1bd6672b5239e337443afd74cd2209d
Null <abc612008@126.com>
2016-10-02 13:26:53 +0800

    buffer

--- qqanalyzer.py
+++ qqanalyzer.py
@@ -2,6 +2,7 @@ import os
 import sys
 import re
 import time
+import pickle
 
 class message:
 	time=""
@@ -15,7 +16,7 @@ class messages:
 	name=""
 	msgs=[]
 	user_qq_name={}
-	def __init__(self, file_content):
+	def __init__(self, file_content, buffer,buffer2, buffer_valid):
 		# define some constants, use the array index
 		GROUP_NAME_LINE=5
 		START_LINE=8
@@ -27,62 +28,72 @@ class messages:
 		# get the group name in the 5th line
 		self.name=(lambda line:line[line.index(":")+1:])(file_content[GROUP_NAME_LINE])
 
-		prog_all=len(file_content)
-		prog_now=START_LINE
-		just_now=False # just after a new message, for optimizing
-		time_analyze=0
+		if buffer_valid:
+			self.msgs=pickle.loads(buffer.read())
+			self.user_qq_name=pickle.loads(buffer2.read())
+		else:
+			# start read messages
+			prog_all=len(file_content)
+			prog_now=START_LINE
+			just_now=False # just after a new message, for optimizing
+			time_analyze=0
 
-		for line in file_content[8:]:
-			prog_now=prog_now+1
-			res=[]
-			if not just_now: res=name_re.findall(line)
-			if not res and not just_now: res=name_email.findall(line)
+			for line in file_content[8:]:
+				prog_now=prog_now+1
+				res=[]
+				if not just_now: res=name_re.findall(line)
+				if not res and not just_now: res=name_email.findall(line)
 
-			if res:
-				# check the time first
-				time_analyze_this=time.mktime(time.strptime(res[0][0], "%Y-%m-%d %H:%M:%S"))
-				if time_analyze_this<time_analyze: continue
-				time_analyze=time_analyze_this
+				if res:
+					# check the time first
+					time_analyze_this=time.mktime(time.strptime(res[0][0], "%Y-%m-%d %H:%M:%S"))
+					if time_analyze_this<time_analyze: continue
+					time_analyze=time_analyze_this
 
-				# remove the blank element and add to the array
-				try: msg.content=msg.content[:-1]; self.msgs.append(msg);
-				except NameError: pass
+					# remove the blank element and add to the array
+					try: msg.content=msg.content[:-1]; self.msgs.append(msg);
+					except NameError: pass
 
-				# create the next message
-				msg=message()
-				msg.time, msg.name, msg.qq=res[0]
-				msg.name=msg.name.replace("\u202e","").replace("\u202d","")
-				msg.time=time_analyze_this;
+					# create the next message
+					msg=message()
+					msg.time, msg.name, msg.qq=res[0]
+					msg.name=msg.name.replace("\u202e","").replace("\u202d","")
+					msg.time=time_analyze_this;
 
-				# add/update the qq-name dict
-				self.user_qq_name[msg.qq]=msg.name
+					# add/update the qq-name dict
+					self.user_qq_name[msg.qq]=msg.name
 
-				just_now=True
-			else:
-				# add to char content array
-				msg.content.append(line.replace("\n",""))
-				just_now=False
+					just_now=True
+				else:
+					# add to char content array
+					msg.content.append(line.replace("\n",""))
+					just_now=False
 
-			if prog_now%10==0: print("\rProgress: %f%%(%d/%d)"%(prog_now/prog_all*100.0,prog_now,prog_all),end="")
+				if prog_now%10==0: print("\rProgress: %f%%(%d/%d)"%(prog_now/prog_all*100.0,prog_now,prog_all),end="")
+			
+			# write to buffer
+			buffer.write(pickle.dumps(self.msgs))
+			buffer2.write(pickle.dumps(self.user_qq_name))
+			
 		print("\rDone"+" "*32)
 
 	def getMessages(self):
 		return self.msgs;
 
+buffer_valid = os.path.exists(sys.argv[1]+".buffer");
 data = open(sys.argv[1],encoding="utf-8")
-msgs = messages(data.readlines())
+buffer = open(sys.argv[1]+".buffer","rb" if buffer_valid else "wb")
+buffer2 = open(sys.argv[1]+".2.buffer","rb" if buffer_valid else "wb")
+msgs = messages(data.readlines(), buffer,buffer2, buffer_valid)
 data.close()
-
+buffer.close()
 # to analyze the relationship between the numbers of messages and days
 def analyze_all(msgs, min_unit):
 	time_start=int(msgs.msgs[0].time//min_unit)
 	stat={}
 	for m in msgs.getMessages():
 		key=int(m.time//min_unit-time_start)
-		try:
-			stat[key]=stat[key]+1
-		except KeyError:
-			stat[key]=1
+		stat[key]=stat.get(key, 0)+1
 	return stat
 
 # to analyze the relationship between the numbers of messages and each day of weeks
@@ -91,24 +102,18 @@ def analyze_week(msgs):
 	stat={}
 	for m in msgs.getMessages():
 		key=int(m.time//CHECK_INV%7)
-		try:
-			stat[key]=stat[key]+1
-		except KeyError:
-			stat[key]=1
+		stat[key]=stat.get(key, 0)+1
 	return stat
 
 # to analyze the numbers of members' messages
 def analyze_user(msgs):
 	stat={}
 	for msg in msgs.getMessages():
-		try:
-			stat[msg.qq]=stat[msg.qq]+1
-		except KeyError:
-			stat[msg.qq]=1
+		stat[msg.qq]=stat.get(msg.qq, 0)+1
 	return stat
 
 def print_plain(out,dic):
-	for (key, value) in stat.items():
+	for (key, value) in dic.items():
 		out.write("%d,%s\n"%(key,value))
 
 def print_uv(out,dic,msgs):
